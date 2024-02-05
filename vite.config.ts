3b159ea1bc2f79f734dc0d81fe981fcc94a373b3
mounica-bashaboyina <mounicabashaboyina@gmail.com>
2024-02-05 20:32:13 +0530

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/content-list/',
-  build: {
-    rollupOptions: {
-      external: ['/content-list/assets/index-T3C7dc7k.js']
-    }
-  },
   plugins: [react()],
 })
